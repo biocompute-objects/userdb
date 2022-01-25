b7f74fe0062ac0d901310c50776556cc19da4341
Hadley King <HadleyKing@users.noreply.github.com>
2022-01-25 07:43:06 -0500

    Update Static files

--- portalusers/settings.py
+++ portalusers/settings.py
@@ -131,13 +131,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = '/api/static/'
+# STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+STATIC_ROOT = '/var/www/bcoeditor/bco_api/bco_api/static/'
 
-STATIC_ROOT = os.path.join(BASE_DIR, "static/")
-
-STATICFILES_DIRS = [
-   # join(BASE_DIR, "static"),
-   ]
 
 
 # Authentication
