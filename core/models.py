96c8b1d80c61f4cac71f94549625610ab9eca646
Hadley King <hadley_king@gwu.edu>
2022-03-17 14:00:45 -0400

    Add Prefix Registry table and data
    Changes to be committed:
    	new file:   core/migrations/0002_prefixes.py
    	modified:   core/models.py
    	new file:   core_prefixes.sql

--- core/models.py
+++ core/models.py
@@ -1,13 +1,15 @@
-# Customer user model.
-# Source: https://docs.djangoproject.com/en/3.1/topics/auth/customizing/#extending-the-existing-user-model
-# Source: https://docs.djangoproject.com/en/3.1/topics/db/models/#many-to-one-relationships
+#!/usr/bin/env python3
+"""Customer user model.
+    Source: https://docs.djangoproject.com/en/3.1/topics/auth/customizing/#extending-the-existing-user-model
+    Source: https://docs.djangoproject.com/en/3.1/topics/db/models/#many-to-one-relationships
+"""
 
 from django.db import models
 from django.contrib.auth.models import User
+from django.core.mail import send_mail
 from django.dispatch import receiver
 from django.urls import reverse
 from django_rest_passwordreset.signals import reset_password_token_created
-from django.core.mail import send_mail
 from rest_framework import status
 from rest_framework.response import Response
 
@@ -16,7 +18,8 @@ def password_reset_token_created(sender, instance, reset_password_token, *args, 
     """
     Create the token for a password reset.
     """
-    email_plaintext_message = "{}?token={}".format(reverse('password_reset:reset-password-request'), reset_password_token.key)
+    email_plaintext_message = "{}?token={}".format(reverse(
+        'password_reset:reset-password-request'), reset_password_token.key)
 
     try:
         send_mail(
@@ -27,52 +30,61 @@ def password_reset_token_created(sender, instance, reset_password_token, *args, 
             recipient_list=[reset_password_token.user.email],
             fail_silently=False,
         )
-        
-    except Exception as e:
+
+    except Exception as error:
         print('activation_link', reset_password_token)
-        # print('ERROR: ', e)
+        # print('ERROR: ', error)
         # TODO: Should handle when the send_mail function fails?
-        # return Response(status=status.HTTP_500_INTERNAL_SERVER_ERROR, data={"message": "Not able to send authentication email: {}".format(e)})
-        return Response(status=status.HTTP_201_CREATED, data={"message": "Account has been requested. Activation email was not sent. Check with your database administrator for activation of your api account"})
+        # return Response(status=status.HTTP_500_INTERNAL_SERVER_ERROR, data={
+        # "message": "Not able to send authentication email: {}".format(error)})
+        return Response(status=status.HTTP_201_CREATED,
+            data={"message": "Reset token has been requested but email was not sent."\
+                f" Check with your database administrator for your token. {error}"})
 
 class Profile(models.Model):
     """
     Modle for storing additional user information
+    Public Profile
+    User Affiliation
+    User ORCID
     """
     username = models.OneToOneField(User, on_delete=models.CASCADE)
-    # Public Profile
     public = models.BooleanField(blank = True, default=False)
-    # User Affiliation
     affiliation = models.CharField(blank = True, max_length = 1000)
-    # User ORCID    
     orcid = models.CharField(blank = True, max_length = 1000)
 
-# API Information is kept separate so that we can use it
-# elsewhere easily.
 
-
-# API Information
 class ApiInfo(models.Model):
+    """API Information
+    API Information is kept separate so that we can use it
+    elsewhere easily.
+    Set the local user.
+    Servers for which the user has keys.
+    The username on the server.
+    max_length = 15 because hostnames are xxx.xxx.xxx.xxx
+    Need to use a human-readable hostname
+    Need to know where to make calls.
+    "Arbitrarily" long token
+    Permissions and other information.
+    """
 
-    # Set the local user.
-    local_username = models.ForeignKey(User, on_delete = models.CASCADE, related_name = 'custom_user')
-    
-    # Servers for which the user has keys.
-
-    # The username on the server.
+    local_username = models.ForeignKey(
+        User,
+        on_delete = models.CASCADE,
+        related_name = 'custom_user'
+    )
     username = models.CharField(blank = True, max_length = 1000)
-
-    # max_length = 15 because hostnames are xxx.xxx.xxx.xxx
     hostname = models.CharField(blank = True, max_length = 15)
-
-    # Need to use a human-readable name
     human_readable_hostname = models.CharField(blank = True, max_length = 1000)
-    
-    # Need to know where to make calls.
     public_hostname = models.CharField(blank = True, max_length = 1000)
-
-    # "Arbitrarily" long token
     token = models.CharField(blank = True, max_length = 1000)
-
-    # Permissions and other information.
     other_info = models.JSONField()
+
+class Prefixes(models.Model):
+    """Prefix Table: core_prefixes
+
+    """
+    username = models.CharField(max_length = 100)
+    prefix = models.CharField(max_length = 5, primary_key=True, unique=True)
+    registration_date = models.DateTimeField()
+    registration_certificate = models.CharField(max_length = 1000)
