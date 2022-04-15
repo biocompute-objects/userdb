7395b3a954d772d5b2ce3cf5c372e8210aad932c
Hadley King <hadley_king@gwu.edu>
2022-04-15 14:47:35 -0400

    Added API refresh to token check Changes to be committed: 	modified:   core/serializers.py 	modified:   core/urls.py 	modified:   portalusers/utils.py

--- core/serializers.py
+++ core/serializers.py
@@ -1,18 +1,18 @@
-from django.db.models.query import QuerySet
+#!/usr/bin/env python3
+"""Custom Serializers
+"""
 from rest_framework import serializers
 from rest_framework_jwt.settings import api_settings
 from django.contrib.auth.models import User
-
-# API model
-from .models import ApiInfo, Profile
+from django.contrib.auth.models import Group
+from core.models import ApiInfo, Profile
 
 # Groups require special processing.
-# Source: https://stackoverflow.com/questions/33844003/how-to-serialize-groups-of-a-user-with-django-rest-framework/33844179
-from django.contrib.auth.models import Group
+# Source: https://stackoverflow.com/questions/33844003/
+#           how-to-serialize-groups-of-a-user-with-django-rest-framework/33844179
 
 class ChangePasswordSerializer(serializers.Serializer):
-    """
-    Serializer for password change endpoint.
+    """Serializer for password change endpoint.
 
     * Provideds serializer for an old password and a new password
 
@@ -21,68 +21,78 @@ class ChangePasswordSerializer(serializers.Serializer):
     """
 
     model = User
-
     old_password = serializers.CharField(required=True)
     new_password = serializers.CharField(required=True)
 
 # Profile serializer
 class ProfileSerializer(serializers.ModelSerializer):
+    """Profile serializer
 
-    # username = serializers.SlugRelatedField(slug_field = 'username', queryset = User.objects.all())
-
-    class Meta:
-        model = Profile
-        fields = ('username', 'public', 'affiliation', 'orcid')
-
-
-
-# Profile serializer
-class ProfileSerializer(serializers.ModelSerializer):
-
-    # username = serializers.SlugRelatedField(slug_field = 'username', queryset = User.objects.all())
+    username = serializers.SlugRelatedField(slug_field = 'username', queryset = User.objects.all())
+    """
 
     class Meta:
+        """Meta"""
         model = Profile
         fields = ('username', 'public', 'affiliation', 'orcid')
-
 
 
 # API serializer
 class ApiSerializer(serializers.ModelSerializer):
+    """API Serializer
 
-    # Only if the username on portal and the API are the same...
-    # username = serializers.SlugRelatedField(slug_field = 'username', queryset = User.objects.all())
-    
+    Only if the username on portal and the API are the same...
+    """
+
     class Meta:
+        """Meta"""
         model = ApiInfo
-        fields = ('username', 'hostname', 'human_readable_hostname', 'public_hostname', 'token', 'other_info',)
+        fields = (
+            'username',
+            'hostname',
+            'human_readable_hostname',
+            'public_hostname',
+            'token',
+            'other_info'
+        )
 
 
 class GroupSerializer(serializers.ModelSerializer):
-    
+    """Group Serializer"""
     class Meta:
+        """Meta"""
         model = Group
         fields = ('name',)
 
 
 class UserSerializer(serializers.ModelSerializer):
-
+    """User Serializer"""
     apiinfo = ApiSerializer(source='custom_user', many=True)
     groups = GroupSerializer(many=True)
     profile = ProfileSerializer(many=False)
 
     class Meta:
+        """Meta"""
         model = User
-        fields = ('username', 'password', 'first_name', 'last_name', 'email', 'profile', 'groups', 'apiinfo')
+        fields = (
+            'username',
+            'password',
+            'first_name',
+            'last_name',
+            'email',
+            'profile',
+            'groups',
+            'apiinfo'
+        )
 
 
 class UserSerializerWithToken(serializers.ModelSerializer):
-
+    """User Serializer"""
     token = serializers.SerializerMethodField()
     password = serializers.CharField(write_only=True)
 
     def get_token(self, obj):
-
+        """Get Token"""
         jwt_payload_handler = api_settings.JWT_PAYLOAD_HANDLER
         jwt_encode_handler = api_settings.JWT_ENCODE_HANDLER
 
@@ -92,7 +102,8 @@ class UserSerializerWithToken(serializers.ModelSerializer):
         return token
 
     def create(self, validated_data):
-
+        """Create
+        """
         password = validated_data.pop('password', None)
         instance = self.Meta.model(**validated_data)
 
@@ -103,5 +114,6 @@ class UserSerializerWithToken(serializers.ModelSerializer):
         return instance
 
     class Meta:
+        "Meta"
         model = User
         fields = ('token', 'username', 'password', 'first_name', 'last_name', 'email', 'groups',)
--- portalusers/utils.py
+++ portalusers/utils.py
@@ -1,21 +1,37 @@
-from core.serializers import UserSerializer, ApiSerializer
+#!/usr/bin/env python3
+"""Utils
+"""
 
+import json
+import requests
+from core.serializers import UserSerializer
+from core.models import ApiInfo
+
+def update_api_info(api):
+    """Update API info"""
+
+    url = api['public_hostname']+'/api/accounts/describe/'
+    token = api['token']
+    header =  {'Authorization': f'Token {token}'}
+    response = requests.post(url, headers=header)
+    return json.loads(response.text)
 
 def my_jwt_response_handler(token, user=None, request=None):
+    """JWT
 
-    # BAD SOLUTION!!!
-    # Couldn't get the groups to work quite right, so a bit hacky here.
+    BAD SOLUTION!!!
+    Couldn't get the groups to work quite right, so a bit hacky here.
+    TODO: refer to API code for a cleaner way to do this.
+    """
+
     user_info = UserSerializer(user, context={'request': request}).data
-    print('user')
-    print(user)
-    print('------')
-    print('user_info')
-    print(user_info)
+    for api in user_info['apiinfo']:
+        updated_api = update_api_info(api)
+        new = ApiInfo.objects.get(token=updated_api['token'])
+        new.other_info = updated_api['other_info']
+        new.save()
+        # print(new.other_info)
 
-    # TODO: refer to API code for a cleaner way to do this.
     user_info['groups'] = [list(i.items())[0][1] for i in user_info['groups']]
 
-    return {
-        'token': token,
-        'user': user_info
-    }
+    return {'token': token, 'user': user_info}
